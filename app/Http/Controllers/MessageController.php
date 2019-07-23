545bed8e1e3587158cc552c1a22faff57c1b5768
tokingfisher <tokingfisher19@gmail.com>
2019-07-23 12:57:18 +0600

    Server Stop Add in backend

--- app/Http/Controllers/MessageController.php
+++ app/Http/Controllers/MessageController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\api_user;
+use App\switch_seting;
 use Illuminate\Support\Collection;
 use App\Operator;
 use App\messagein;
@@ -34,6 +36,21 @@ class MessageController extends Controller
            // ->groupBy('Gateway')
             ->get();
 
+        $errorCounts = messagelog::select('ErrorCode', 'Gateway','Id', DB::raw('count("Id") as total'))
+                        ->where('ErrorCode', '>', '0')
+                        ->where('StatusCode','!=','201')
+                        ->orderBy('SendTime', 'desc')
+                        ->groupBy('ErrorCode', 'Gateway')
+                        ->get();
+        if(!empty($errorCounts)){
+            foreach($errorCounts as $errorCount){
+                $e_count = switch_seting::where('ErrorCode', $errorCount->ErrorCode)->first()->value('Limit');
+                if($e_count<= $errorCount->total){
+                    $this->serverStop('stop',$errorCount->Gateway);
+                }
+            }
+        }
+
 
         return view('home',
                         compact(
@@ -48,6 +65,22 @@ class MessageController extends Controller
                
     }
 
+    private function serverStop($command='stop', $gateway){
+        $apiUser = api_user::first();
+        $headers = array(
+            'Content-Type: application/json',
+            'Accept:application/json',
+        );
+//        $url = 'http://209.126.105.236:9710/http/send-server-command?username='.$apiUser->UserName.'&password='.$apiUser->Password.'&command='.$command.'-gateway&gateway='.$gateway.'';
+        $url = 'http://209.126.105.236:9710/http/send-server-command?username=admin&password=admin123654&command=stop-gateway&gateway=SMPP GW-DTT';
+        $curl = curl_init($url);
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $headers);
+        curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+        $curl = curl_exec($curl);
+
+        $result =  json_decode($curl);
+        dd($result);
+    }
    
     public function ResetError(Request $request)
     {
